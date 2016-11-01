81d2f45da90ddd7a2b802738e53fb7c05c43e4f7
Sam Jo <samjo891@gmail.com>
2016-11-01 19:19:34 -0400

    Start global leaderboard with firebase

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,20 @@ module.exports = {
   output: {
   	filename: "./lib/bundle.js"
   },
+  module: {
+    loaders: [
+      {
+        test: [/\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
   devtool: 'source-map',
   resolve: {
-    extensions: ["", ".js"]
+    extensions: ["",".js"]
   }
 };
