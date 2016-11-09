fff9107274a001cca4804e08b86da448eeaf79f3
Sam Jo <samjo891@gmail.com>
2016-11-08 20:19:51 -0500

    Write tests for collision detection

--- test/util_test.js
+++ test/util_test.js
@@ -14,4 +14,17 @@ describe('Util', function() {
       assert.equal(Util.distanceBetween(objectA, objectC), 0);
     });
   });
+
+  describe('#isCrash(objectA, objectB)', function() {
+    const objectA = { pos: [0, 0], radius: 1 };
+    const objectB = { pos: [0, 3], radius: 1 };
+    const objectC = { pos: [0, 0], radius: 1 };
+
+    it('should return false if objects are not touching', function() {
+      assert.equal(Util.isCrash(objectA, objectB), false);
+    });
+    it('should return true if objects are touching', function() {
+      assert.equal(Util.isCrash(objectA, objectC), true);
+    });
+  });
 });
